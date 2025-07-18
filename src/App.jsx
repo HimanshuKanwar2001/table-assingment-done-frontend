f744c5c9bf4e8d5a17afccff20125c56920adc89
himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-07-18 21:38:03 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Filters from "./components/Filters";
 import ProductTable from "./components/ProductTable";
-import { data } from "./data/data";
+import { Atom } from "react-loading-indicators"; 
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -11,6 +11,7 @@ const App = () => {
     price: "All",
     status: "All",
   });
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     fetch("https://api.udhhyog.com/v1/test")
@@ -18,10 +19,12 @@ const App = () => {
       .then((data) => {
         setProducts(data);
         setFiltered(data);
+        setLoading(false); 
       })
-      .catch((err) => console.error("API Error:", err));
-    // setProducts(data);
-    // setFiltered(data);
+      .catch((err) => {
+        console.error("API Error:", err);
+        setLoading(false); 
+      });
   }, []);
 
   useEffect(() => {
@@ -50,8 +53,21 @@ const App = () => {
   return (
     <div className="p-6 font-poppins bg-gray-50 min-h-screen">
       <h1 className="text-2xl font-semibold mb-4">Product Inventory</h1>
-      <Filters filters={filters} setFilters={setFilters} products={products} />
-      <ProductTable data={filtered} />
+
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <Atom color="#fbbf24" size={60} text="" />
+        </div>
+      ) : (
+        <>
+          <Filters
+            filters={filters}
+            setFilters={setFilters}
+            products={products}
+          />
+          <ProductTable data={filtered} />
+        </>
+      )}
     </div>
   );
 };
